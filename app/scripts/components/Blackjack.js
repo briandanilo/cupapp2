d7e6860b8202600edc68e25579774af8e3fd3424
bdanilo <brian.danilo@gmail.com>
2020-03-29 17:47:27 -0700

    bj works mostly

--- app/scripts/components/Blackjack.js
+++ app/scripts/components/Blackjack.js
@@ -17,13 +17,12 @@ class Blackjack extends React.Component {
     super(props);
     this.state = {
       currentGame: new Game(),
-      defaultBet: 10
+      betAmount: 10
     }
   }
 
 
   buttonClick = (e) => {
-
     switch (e.target.value) {
       case "deal":
         this.state.currentGame.dispatch(actions.deal())
@@ -42,25 +41,64 @@ class Blackjack extends React.Component {
         this.setState({ currentGame: this.state.currentGame });
         break
       case "double":
-        this.state.currentGame.dispatch(actions.split({position:"right"}))
+        this.state.currentGame.dispatch(actions.double({position:"right"}))
         this.setState({ currentGame: this.state.currentGame });
         break
       case "reset":
         this.setState({currentGame: new Game()})
         break
     }
+  }
 
-    console.log("game state after button click: :",this.state.currentGame.getState())
-
+  splitHandButtonClick = (e) => {
+    switch (e.target.value) {
+      case "hit":
+        this.state.currentGame.dispatch(actions.hit({position:"left"}))
+        this.setState({ currentGame: this.state.currentGame });
+        break
+      case "stand":
+        this.state.currentGame.dispatch(actions.stand({position:"left"}))
+        this.setState({ currentGame: this.state.currentGame });
+        break
+      case "split":
+        this.state.currentGame.dispatch(actions.split({position:"left"}))
+        this.setState({ currentGame: this.state.currentGame });
+        break
+      case "double":
+        this.state.currentGame.dispatch(actions.double({position:"left"}))
+        this.setState({ currentGame: this.state.currentGame });
+        break
+    }
   }
 
   renderPlayerCards(){
-    if (this.state.currentGame.state.handInfo.right.cards){
+    if (this.state.currentGame.state.handInfo.right.cards && !this.state.currentGame.state.handInfo.left.cards){
       let handArray = this.state.currentGame.state.handInfo.right.cards
       return handArray.map((i)=>{
         return <p>{i.text},</p>
       })
+    } else if (this.state.currentGame.state.handInfo.right.cards && this.state.currentGame.state.handInfo.left.cards) {
+      let handArrayRight = this.state.currentGame.state.handInfo.right.cards
+      let handArrayLeft = this.state.currentGame.state.handInfo.left.cards
+      return (<div>
+        <p>Hand 1:{this.renderSplitCards(handArrayRight)}</p>
+        <button className="btn btn-default tools-btn" value="hit" onClick={this.buttonClick}>Hit</button>
+        <button className="btn btn-default tools-btn" value="stand" onClick={this.buttonClick}>Stand</button>
+        <button className="btn btn-default tools-btn" value="double" onClick={this.buttonClick}>Double</button>
+        <button className="btn btn-default tools-btn" value="split" onClick={this.buttonClick}>Split</button>
+        <p>Hand 2:{this.renderSplitCards(handArrayLeft)}</p>
+        <button className="btn btn-default tools-btn" value="hit" onClick={this.splitHandButtonClick}>Hit</button>
+        <button className="btn btn-default tools-btn" value="stand" onClick={this.splitHandButtonClick}>Stand</button>
+        <button className="btn btn-default tools-btn" value="double" onClick={this.splitHandButtonClick}>Hit</button>
+        <button className="btn btn-default tools-btn" value="split" onClick={this.splitHandButtonClick}>Stand</button>
+      </div>)
     }
+  }
+
+  renderSplitCards(handArray){
+    return handArray.map((i)=>{
+      return <p>{i.text},</p>
+    })
   }
 
   renderDealerCards(){
@@ -83,16 +121,28 @@ class Blackjack extends React.Component {
     if (this.state.currentGame.state.initialBet)
       return <p>Bet: {this.state.currentGame.state.initialBet}</p>
     else
-      return <p>Bet: {this.state.defaultBet}</p>
+      return <p>Bet: {this.state.betAmount}</p>
   }
 
   renderOutcome(){
-    if (this.state.currentGame.state.stage =="done")
-      return <p>Game Over</p>
+    if (this.state.currentGame.state.stage =="done"){
+      let risk = this.state.currentGame.state.finalBet
+      let win = this.state.currentGame.state.wonOnRight + this.state.currentGame.state.wonOnLeft
+      return <p>Game Over.  Your net: {win - risk}</p>
+    }
+  }
+
+  renderSplitHand(){
+    if (this.state.currentGame.state.handInfo.left.cards){
+      let handArray = this.state.currentGame.state.handInfo.left.cards
+      return handArray.map((i)=>{
+            return <p>{i.text},</p>
+      })
+    }
   }
 
   render() {
-
+    console.log("game state at render: :",this.state.currentGame.getState())
     return (<div><Link to="/">Back to Homepage</Link>
       <div className="Component">
         <h1>Blackjack</h1>
